8114812ba3de99079cab4cce2ee5fead0158e868
Eduardo Renteria <98445667+PozolitoVerde@users.noreply.github.com>
2023-01-13 14:40:42 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi practica con code sandbox</h1>
+<div>
+  <h2>Renteria Meza Angel Eduardo</h2>
+  Universidad Tecnologica de Durango
+  <a href="https://github.com/PozolitoVerde/AppReact.git" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
